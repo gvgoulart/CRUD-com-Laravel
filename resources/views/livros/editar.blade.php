784e4b7e970b657f64dee02e7979ce60c9b92d8c
Gabriel Goulart <gabriel.vieira@saudegarantida.com.br>
2021-07-08 17:55:35 -0300

    Edição de livros

---
+++ resources/views/livros/editar.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post"></form>
+    @csrf
+        <div><label for="isbn">ISBN</label></div><input type="text">
+        <div><label for="nome">NOME</label></div><input type="text">
+        <div><label for="custo">AUTOR</label></div><input type="text">
+        <div><label for="preco">PREÇO</label></div><input type="text">
+        <button type="submit">SALVAR</button>
+</body>
+</html>
